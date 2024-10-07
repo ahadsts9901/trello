5b07dd77b9bc24ba1033d0847269eb1672cb250f
ahadsts9901 <ahadsts990@gmail.com>
2024-10-07 15:23:01 +0500

    boards-schema

---
+++ backend/models/boardModel.mjs
@@ -0,0 +1,54 @@
+import { Schema, model } from "mongoose";
+
+// {
+//     "_id": "IbJLYuptc",
+//     "name": "new",
+//     "createdBy": "7iXaRpAeS",
+//     "backgroundImage": "https://images.unsplash.com/photo-1470813740244-df37b8c1edcb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NTg1MDd8MHwxfHNlYXJjaHwzfHxuYXR1cmV8ZW58MHwwfHx8MTcyNzQ0MDcyOXww&ixlib=rb-4.0.3&q=80&w=1080",
+//     "users": []
+//     "dateCreated": "9/27/2024, 5:26:39 PM",
+//   }
+
+let boardSchema = new Schema({
+    boardName: {
+        type: String,
+        maxlength: 120,
+        trim: true,
+        required: true,
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+        required: true,
+    },
+    backgroundImage: {
+        type: String,
+        maxlength: 1000,
+        trim: true,
+        required: true,
+    },
+    users: {
+        type: [Schema.Types.ObjectId],
+        ref: "users",
+        default: [],
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+boardSchema.pre('save', function (next) {
+    if (this?.email) this.email = this?.email?.toLowerCase()
+    next();
+});
+
+let boardModel;
+
+try {
+    boardModel = model('boards');
+} catch (error) {
+    boardModel = model('boards', boardSchema);
+}
+
+export { boardModel };
